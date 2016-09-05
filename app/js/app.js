9ee44a31de509c3d5a9718e8e63343de002ff9e1
Score, Robert <rscore@dresources.com>
2016-09-05 08:37:38 +0100

    completed - using angulars $cookies service

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // this is creating the module
-var eventsApp = angular.module('eventsApp', ['ngResource'])
+var eventsApp = angular.module('eventsApp', ['ngResource', /*'ngCookies'*/ ])
     .factory('myCache', function($cacheFactory) {
         return $cacheFactory('myCache', {
             capacity: 3
