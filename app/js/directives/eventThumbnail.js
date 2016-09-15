a6dd1b50dc667bf1ee8f6b3640f078f213335225
Score, Robert <rscore@dresources.com>
2016-09-15 08:38:23 +0100

    completed - Isolating Directive Scope

--- app/js/directives/eventThumbnail.js
+++ app/js/directives/eventThumbnail.js
@@ -4,7 +4,10 @@ eventsApp.directive('eventThumbnail', function() {
     return {
         restrict: 'E',
         replace: true, // will remove/replace the directive element from the html ...eg the <event-thumbail> bit with the html from the template
-        templateUrl: '/templates/directives/eventThumbnail.html'
+        templateUrl: '/templates/directives/eventThumbnail.html',
+        scope: { // this is used to enable multiple versions of the same directive to have their own specific binding
+            event: '='
+        }
     }
 
 
--- app/js/directives/mySample.js
+++ app/js/directives/mySample.js
@@ -4,7 +4,9 @@ eventsApp.directive('mySample', function($compile) {
     return {
         restrict: 'E',
         template: "<input type='text' ng-model='sampleData' />{{sampleData}}</br>" // this will get compiled automaically withouth having to manually use $compile
+        scope: {
 
+        }
 
         // link: function(scope, element, attrs, controller) {
         //     var markup = "<input type='text' ng-model='sampleData' />{{sampleData}}</br>";
